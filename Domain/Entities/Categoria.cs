83762db3488b4ff59040ea493700ac0e1cd3fb6e
gmadalosso <gbmadalosso@gmail.com>
2026-01-12 23:00:51 -0300

    criada entidade Categoria

---
+++ Domain/Entities/Categoria.cs
@@ -0,0 +1,38 @@
+using ControleGastos.Api.Domain.Enums;
+
+namespace ControleGastos.Api.Domain.Entities;
+
+public class Categoria
+{
+    public Guid Id { get; private set; }
+    public string Descricao { get; private set; } = string.Empty;
+    public Finalidade Finalidade { get; private set; }
+
+    public Categoria(string descricao, Finalidade finalidade)
+    {
+        if (string.IsNullOrWhiteSpace(descricao))
+            throw new ArgumentException("Descrição da categoria é obrigatória", nameof(descricao));
+
+        Id = Guid.NewGuid();
+        Descricao = descricao;
+        Finalidade = finalidade;
+    }
+
+    public bool PermiteTipo(TipoTransacao tipo)
+    {
+        switch (Finalidade)
+        {
+            case Finalidade.Ambas:
+                return true;
+
+            case Finalidade.Despesa:
+                return tipo == TipoTransacao.Despesa;
+
+            case Finalidade.Receita:
+                return tipo == TipoTransacao.Receita;
+
+            default:
+                return false;
+        }
+    }
+}
