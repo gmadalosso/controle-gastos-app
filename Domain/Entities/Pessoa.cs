033e65eb0b8f3d0106c468a0bc5543cce8fc137f
gmadalosso <gbmadalosso@gmail.com>
2026-01-12 22:51:16 -0300

    criada entidade Pessoa

---
+++ Domain/Entities/Pessoa.cs
@@ -0,0 +1,25 @@
+namespace ControleGastos.Api.Domain.Entities;
+
+public class Pessoa
+{
+    public Guid Id { get; private set; }
+    public string Nome { get; private set; } = string.Empty;
+    public int Idade { get; private set; }
+
+    public Pessoa(string nome, int idade)
+    {
+        if (string.IsNullOrWhiteSpace(nome))
+        {
+            throw new ArgumentException("Nome não pode ser vazio.", nameof(nome));
+        }
+
+        if (idade < 0)
+        {
+            throw new ArgumentOutOfRangeException("Idade não pode ser menor do que zero.", nameof(idade));
+        }
+
+        Id = Guid.NewGuid();
+        Nome = nome;
+        Idade = idade;
+    }
+}
