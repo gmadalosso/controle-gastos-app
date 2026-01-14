c70424bf7d52e4907fc0ce0368e8c10ac8688f70
gmadalosso <gbmadalosso@gmail.com>
2026-01-14 09:00:12 -0300

    adicionadas rotas e main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,15 +7,13 @@ import { Transacoes } from "./pages/Transacoes";
 function App() {
   return (
     <BrowserRouter>
-      <div style={{ padding: "1rem" }}>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/pessoas" element={<Pessoas />} />
-          <Route path="/categorias" element={<Categorias />} />
-          <Route path="/transacoes" element={<Transacoes />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/pessoas" element={<Pessoas />} />
+        <Route path="/categorias" element={<Categorias />} />
+        <Route path="/transacoes" element={<Transacoes />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </BrowserRouter>
   );
 }
